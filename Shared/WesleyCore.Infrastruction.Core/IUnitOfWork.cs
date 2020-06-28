e1e7a2ef4748a464104fa44c112a0e31c72fa2ed
guanwanliWesley <564232943+guanwanliWesley@users.noreply.github.com>
2020-06-28 17:35:54 +0800

    123
    123

--- Shared/WesleyCore.Infrastruction.Core/IUnitOfWork.cs
+++ Shared/WesleyCore.Infrastruction.Core/IUnitOfWork.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace WesleyCore.Infrastruction.Core
 {
+    /// <summary>
+    /// 工作单元
+    /// </summary>
     public interface IUnitOfWork : IDisposable
     {
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
