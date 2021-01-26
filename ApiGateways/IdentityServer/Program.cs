703b2821bb67f680c121f73b2573c0e6425505f1
Wesley <564232943+guanwanliWesley@users.noreply.github.com>
2021-01-26 14:42:12 +0800

    log4net补充
    log4net补充

--- ApiGateways/IdentityServer/Program.cs
+++ ApiGateways/IdentityServer/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System.IO;
 
 namespace IdentityServer
@@ -27,6 +28,16 @@ namespace IdentityServer
         /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+              .ConfigureLogging((hostingContext, loggingbuilder) =>
+              {
+                  loggingbuilder.AddFilter("System", LogLevel.Warning); //过滤掉系统默认的一些日志
+                  loggingbuilder.AddFilter("Microsoft", LogLevel.Warning);//过滤掉系统默认的一些日志
+                  loggingbuilder.ClearProviders();
+                  loggingbuilder.AddConsole();
+                  loggingbuilder.AddDebug();
+                  //不带参数：表示log4net.config的配置文件就在应用程序根目录下，也可以指定配置文件的路径
+                  loggingbuilder.AddLog4Net();
+              })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
